4ee93c7f26ded2e4d9cffce7e19511b48da14776
José Medeiros <joseprsm@gmail.com>
2024-02-08 20:03:07 +0000

    return data

--- mar/nn/workflow/transform/__main__.py
+++ mar/nn/workflow/transform/__main__.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 
-def transform():
-    pass
+def transform(data: str, schema: str, format: str, **options):
+    data: pd.DataFrame = getattr(pd, f'read_{format}')(data)
+    return data
 
 
 if __name__ == '__main__':
