4e9cd31cc422f9878bb048fd99f508818e28ad7b
José Medeiros <joseprsm@gmail.com>
2024-02-08 23:47:42 +0000

    add click options

--- src/mar/workflow/transform/__main__.py
+++ src/mar/workflow/transform/__main__.py
@@ -1,7 +1,12 @@
+import click
 import pandas as pd
 
 
-def transform(data: str, schema: str, format: str, **options):
+@click.command
+@click.option("--data")
+@click.option("--schema")
+@click.option("--format")
+def transform(data: str, schema: str, format: str):
     data: pd.DataFrame = getattr(pd, f"read_{format}")(data)
     return data
 
