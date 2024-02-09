6de9cdccf6891a9085c9a6b8316321ecf8c0352c
José Medeiros <joseprsm@gmail.com>
2024-02-09 23:35:17 +0000

    get ids, vectors from transform

--- src/mar/workflow/transform/__main__.py
+++ src/mar/workflow/transform/__main__.py
@@ -11,8 +11,7 @@ from mar.workflow.transform import Transform
 @click.option("--target")
 def transform(data: str, schema: str, format: str = None):
     data: pd.DataFrame = getattr(pd, f"read_{format}")(data)
-    vectors = Transform(schema).fit_transform(data)
-
+    ids, vectors = Transform(schema)(data)
 
 
 if __name__ == "__main__":
