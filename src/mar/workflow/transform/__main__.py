88ea2fbb263f066960937685ee5ad5a64bf37feb
José Medeiros <joseprsm@gmail.com>
2024-02-09 23:08:26 +0000

    get vectors

--- src/mar/workflow/transform/__main__.py
+++ src/mar/workflow/transform/__main__.py
@@ -1,14 +1,18 @@
 import click
 import pandas as pd
 
+from mar.workflow.transform import Transform
+
 
 @click.command
 @click.option("--data")
 @click.option("--schema")
 @click.option("--format")
-def transform(data: str, schema: str, format: str):
+@click.option("--target")
+def transform(data: str, schema: str, format: str = None):
     data: pd.DataFrame = getattr(pd, f"read_{format}")(data)
-    return data
+    vectors = Transform(schema).fit_transform(data)
+
 
 
 if __name__ == "__main__":
