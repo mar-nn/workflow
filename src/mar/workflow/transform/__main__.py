41818d406ce29aab4cc753738d511a74362f1636
José Medeiros <joseprsm@gmail.com>
2024-02-10 00:06:09 +0000

    create qdrant collection

--- src/mar/workflow/transform/__main__.py
+++ src/mar/workflow/transform/__main__.py
@@ -1,8 +1,10 @@
 import os
 import click
 import pandas as pd
+from uuid import uuid4
 
 from qdrant_client import QdrantClient
+from qdrant_client.http.models import VectorParams
 
 from mar.workflow.transform import Transform
 
@@ -12,14 +14,21 @@ QDRANT_PORT = os.environ["QDRANT_PORT"]
 qdrant = QdrantClient(url=QDRANT_URL, port=QDRANT_PORT)
 
 
+def create_collection(size: int):
+    name = str(uuid4())
+    config = VectorParams(size=size)
+    qdrant.create_collection(name, config)
+
+
 @click.command
 @click.option("--data")
 @click.option("--schema")
 @click.option("--format")
-@click.option("--target")
 def transform(data: str, schema: str, format: str = None):
     data: pd.DataFrame = getattr(pd, f"read_{format}")(data)
     ids, vectors = Transform(schema)(data)
+
+    create_collection(size=vectors.shape[1])
 
 
 if __name__ == "__main__":
