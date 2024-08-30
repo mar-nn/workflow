5bbde9f2084c82093ad88e2ed3c473a6befb30ed
José Medeiros <joseprsm@gmail.com>
2024-08-30 22:46:22 +0100

    use schema attributes

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -4,21 +4,21 @@ import numpy as np
 from mar.nn.workflow import Transform
 
 
-@pytest.mark.parametrize("target", ["user", "item"])
+@pytest.mark.parametrize("target", ["users", "items"])
 def test_fit(schema, data, target):
-    transformer = Transform(schema["datasets"][target])
+    transformer = Transform(getattr(schema.datasets, target))
     transformer.fit(data)
 
-    assert len(transformer.transformers) == len(schema["datasets"][target])
+    assert len(transformer.transformers) == len(getattr(schema.datasets, target))
 
 
-@pytest.mark.parametrize("target", ["user", "item"])
+@pytest.mark.parametrize("target", ["users", "items"])
 def test_transform(schema, data, target):
-    transformer = Transform(schema["datasets"][target])
+    transformer = Transform(getattr(schema.datasets, target))
     ids, features = transformer.fit(data).transform(data)
 
     assert ids.shape == (data.shape[0], 1)
-    assert len(np.unique(ids)) == data.loc[:, f"{target}Id"].nunique()
+    assert len(np.unique(ids)) == data.loc[:, f"{target[:-1]}Id"].nunique()
 
     assert len(features.shape) == 2
     assert features.shape[0] == data.shape[0]
