95eee81e280c8ce4ef343390fb89bae13f706d8b
José Medeiros <joseprsm@gmail.com>
2024-08-30 22:59:19 +0100

    test schema initialization

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,4 +1,27 @@
+import pytest
+from pydantic import ValidationError
+
 from mar.nn.workflow.schema import Schema
+
+
+def test_init():
+    schema = Schema(datasets={"interactions": {"foo": "bar"}}, model={"type": "test"})
+
+    assert schema.datasets.interactions == {"foo": "bar"}
+    assert schema.model.type == "test"
+    assert schema.datasets.users is None
+    assert schema.datasets.items is None
+    assert schema.model.hyperparameters is None
+
+
+def test_init_no_interactions():
+    with pytest.raises(ValidationError):
+        Schema(datasets=None, model={"type": "foo"})
+
+
+def test_init_no_model_type():
+    with pytest.raises(ValidationError):
+        Schema(datasets={"interactions": {"foo": "bar"}}, model=None)
 
 
 def test_from_yaml():
