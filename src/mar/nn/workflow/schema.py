e0a1d36c399db934642b542416cbd2590cc54e65
José Medeiros <joseprsm@gmail.com>
2024-08-30 17:21:00 +0100

    refactor pydantic models

--- src/mar/nn/workflow/schema.py
+++ src/mar/nn/workflow/schema.py
@@ -1,17 +1,30 @@
+import json
+
+import yaml
 from pydantic import BaseModel
 
 
-class DatasetsConfig(BaseModel):
+class _Datasets(BaseModel):
     interactions: dict
     users: dict = None
     items: dict = None
 
 
-class ModelConfig(BaseModel):
+class _Model(BaseModel):
     type: str
-    hyperparameters: dict
+    hyperparameters: dict = None
 
 
 class Schema(BaseModel):
-    datasets: DatasetsConfig
-    model: dict
+    datasets: _Datasets
+    model: _Model
+
+    @classmethod
+    def from_yaml(cls, path: str):
+        with open(path, "r") as f:
+            return Schema(**yaml.safe_load(f))
+
+    @classmethod
+    def from_json(cls, path: str):
+        with open(path, "r") as f:
+            return Schema(**json.load(f))
