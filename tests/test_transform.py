bb3e9d358e38e220571ad7dfab9190738f1a54de
José Medeiros <joseprsm@gmail.com>
2024-09-15 21:29:21 +0100

    filter datasets array

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -4,17 +4,23 @@ import pytest
 from mar.nn.workflow import Transform
 
 
+def _filter(datasets: dict, target):
+    return list(filter(lambda ds: ds.name == target, datasets))[0]
+
+
 @pytest.mark.parametrize("target", ["users", "items"])
 def test_fit(schema, data, target):
-    transformer = Transform(getattr(schema.datasets, target))
+    transformer = Transform(_filter(schema.datasets, target))
     transformer.fit(data)
 
-    assert len(transformer.transformers) == len(getattr(schema.datasets, target))
+    assert len(transformer.transformers) == len(
+        _filter(schema.datasets, target).features
+    )
 
 
 @pytest.mark.parametrize("target", ["users", "items"])
 def test_transform(schema, data, target):
-    transformer = Transform(getattr(schema.datasets, target))
+    transformer = Transform(_filter(schema.datasets, target))
     ids, features = transformer.fit(data).transform(data)
 
     assert ids.shape == (data.shape[0], 1)
@@ -28,7 +34,7 @@ def test_transform(schema, data, target):
 
 @pytest.mark.parametrize("target", ["users", "items"])
 def test_call(schema, data, target):
-    transformer = Transform(getattr(schema.datasets, target))
+    transformer = Transform(_filter(schema.datasets, target))
     ids, features = transformer(data)
 
     assert ids.shape == (data.shape[0], 1)
