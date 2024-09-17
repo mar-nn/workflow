9188a53d0753b2bf8fce46cbe7bd4666d7c079d1
José Medeiros <joseprsm@gmail.com>
2024-09-17 22:47:50 +0100

    add argocd helm release

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,10 @@ resource "helm_release" "argo-workflows" {
   chart      = "argo/argo-workflows"
   namespace  = "argo"
 }
+
+resource "helm_release" "argo-cd" {
+  name       = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo/argo-cd"
+  namespace  = "argocd"
+}
